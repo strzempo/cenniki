9ab1b8e28516337462aeab016b034c15b14d65ec
Kamil Strzempowicz <konserw@gmail.com>
2016-11-20 20:49:54 +0100

    fixed ms runetime paths

--- package_script.bat
+++ package_script.bat
@@ -1,12 +1,18 @@
+dir
+dir build 
+dir build/release
+dir release
 mkdir cenniki 
 cd cenniki
 cp ../build/Release/cenniki.exe ./
 REM cp ../build/Logger/Release/logger.dll ./bin/
 REM cp ../build/MainWindow/*.qm ./bin/
-cp ../LICENSE.md ./
+cp ../LICENSE ./
 cp ../README.md ./
+cp "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\concrt140.dll" ./
 cp "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\msvcp140.dll" ./
-cp "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\msvcr140.dll" ./
+cp "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\vccorlib140.dll" ./
+cp "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\vcrunetime140.dll" ./
 cp C:\Qt/5.7/msvc2015/bin/Qt5Core.dll ./
 cp C:\Qt/5.7/msvc2015/bin/Qt5Gui.dll ./
 cp C:\Qt/5.7/msvc2015/bin/Qt5Widgets.dll ./
