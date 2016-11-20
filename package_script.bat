0ee43f999cf54785a0780eef4658c36803fa242a
Kamil Strzempowicz <konserw@gmail.com>
2016-11-20 21:57:05 +0100

    switch back to msvc 2013

--- package_script.bat
+++ package_script.bat
@@ -5,15 +5,13 @@ REM cp ../build/Logger/Release/logger.dll ./bin/
 REM cp ../build/MainWindow/*.qm ./bin/
 cp ../LICENSE ./
 cp ../README.md ./
-cp "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\concrt140.dll" ./
-cp "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\msvcp140.dll" ./
-cp "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\vccorlib140.dll" ./
-cp "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\redist\x86\Microsoft.VC140.CRT\vcruntime140.dll" ./
-cp C:\Qt/5.7/msvc2015/bin/Qt5Core.dll ./
-cp C:\Qt/5.7/msvc2015/bin/Qt5Gui.dll ./
-cp C:\Qt/5.7/msvc2015/bin/Qt5Widgets.dll ./
-cp C:\Qt/5.7/msvc2015/bin/Qt5Quick.dll ./
+cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll" ./
+cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll" ./
+cp C:\Qt/5.7/msvc2013/bin/Qt5Core.dll ./
+cp C:\Qt/5.7/msvc2013/bin/Qt5Gui.dll ./
+cp C:\Qt/5.7/msvc2013/bin/Qt5Widgets.dll ./
+cp C:\Qt/5.7/msvc2013/bin/Qt5Quick.dll ./
 mkdir platforms
-cp C:\Qt/5.7/msvc2015/plugins/platforms/qwindows.dll ./platforms/
+cp C:\Qt/5.7/msvc2013/plugins/platforms/qwindows.dll ./platforms/
 cd ..
 7z a cenniki.7z cenniki
