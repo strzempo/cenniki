c9e68d6ec54237c959ba16dbf03d3664ef8a01bc
Kamil Strzempowicz <konserw@gmail.com>
2016-11-22 22:38:48 +0100

    fix robocopy

--- package_script.bat
+++ package_script.bat
@@ -15,6 +15,6 @@ cp C:\Qt/5.7/msvc2013/bin/Qt5Qml.dll ./
 cp C:\Qt/5.7/msvc2013/bin/Qt5Network.dll ./
 mkdir platforms
 cp C:\Qt/5.7/msvc2013/plugins/platforms/qwindows.dll ./platforms/
-robocopy C:\Qt/5.7/msvc2013/plugins/qml ./ /s /e /y
+robocopy C:\Qt/5.7/msvc2013/plugins/qml ./ /s /e 
 cd ..
 7z a cenniki.7z cenniki
