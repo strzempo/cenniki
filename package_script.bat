50ab6f5ea00c36126ffeb33fb0d3ed0a6625f73a
Kamil Strzempowicz <konserw@gmail.com>
2016-11-22 21:26:49 +0100

    add qml plugin directory to packagescript

--- package_script.bat
+++ package_script.bat
@@ -15,5 +15,6 @@ cp C:\Qt/5.7/msvc2013/bin/Qt5Qml.dll ./
 cp C:\Qt/5.7/msvc2013/bin/Qt5Network.dll ./
 mkdir platforms
 cp C:\Qt/5.7/msvc2013/plugins/platforms/qwindows.dll ./platforms/
+robocopy cp C:\Qt/5.7/msvc2013/plugins/qml ./ /s /e /y
 cd ..
 7z a cenniki.7z cenniki
