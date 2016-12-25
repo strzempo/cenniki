96bb5d57d76b3fbdfaecb1cc23ffb2b104bbc460
Kamil Strzempowicz <konserw@gmail.com>
2016-12-25 11:21:43 +0100

    add more dependencies

--- package_script.bat
+++ package_script.bat
@@ -7,6 +7,7 @@ cp ../LICENSE ./
 cp ../README.md ./
 cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll" ./
 cp "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll" ./
+cp C:\Libraries/boost_1_58_0/lib32-msvc-12.0/boost_serialization-vc120-mt-1_58.dll ./
 cp C:\Qt/5.7/msvc2013/bin/Qt5Core.dll ./
 cp C:\Qt/5.7/msvc2013/bin/Qt5Gui.dll ./
 cp C:\Qt/5.7/msvc2013/bin/Qt5Widgets.dll ./
@@ -15,6 +16,9 @@ cp C:\Qt/5.7/msvc2013/bin/Qt5Qml.dll ./
 cp C:\Qt/5.7/msvc2013/bin/Qt5Network.dll ./
 cp C:\Qt/5.7/msvc2013/bin/Qt5QuickTemplates2.dll ./
 cp C:\Qt/5.7/msvc2013/bin/Qt5QuickControls2.dll ./
+cp C:\Qt/5.7/msvc2013/bin/libEGL.dll ./
+cp C:\Qt/5.7/msvc2013/bin/libGLESv2.dll ./
+cp C:\Qt/5.7/msvc2013/bin/d3dcoompiler_47.dll ./
 mkdir platforms
 cp C:\Qt/5.7/msvc2013/plugins/platforms/qwindows.dll ./platforms/
 mkdir QtQml
